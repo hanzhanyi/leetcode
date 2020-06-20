56c61c85486ca783ef2977cbee493eef95ff43fc
hanzhanyi <hanzhanyi@outlook.com>
2020-06-20 13:53:13 +0800

    2020-06-20

---
+++ src/main/java/com/hanzhanyi/leetcode/DP/problem63.java
@@ -0,0 +1,30 @@
+package com.hanzhanyi.leetcode.DP;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ */
+public class problem63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int dp[][] = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0) {
+                    dp[i][j] = dp[0][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][0];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+}
